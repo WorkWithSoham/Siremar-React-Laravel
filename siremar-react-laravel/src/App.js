0a9db8f03a83c63520c572842a17fe4ab1c3f92c
WorkWithSoham <sohamtembe98@gmail.com>
2022-03-05 17:07:06 -0600

    Routing updated. Packages installed. Dashboard updated.

--- siremar-react-laravel/src/App.js
+++ siremar-react-laravel/src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import './App.css';
-import Home from './components/Home';
-//import Navbar from './components/Navbar';
-import Login from "./components/Login"
-import {Routes, Route } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-   return (
-    <Routes>
-     <Route path="/" exact element={<Home />} />
-     <Route path="/login" element={<Login />} />
-     </Routes>
-   );
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        {/* <Route exact path="/contact" element={<Contact />} /> */}
+        {/* <Route exact path="/aboutus" element={<AboutUs />} /> */}
+        <Route exact path="/dashboard" element={<Dashboard />} />
+
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
