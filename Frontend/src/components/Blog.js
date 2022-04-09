f99ef8ed4d93c862f03102b9d75eb50ff49dc564
WorkWithSoham <sohamtembe98@gmail.com>
2022-04-09 14:55:06 -0500

    wordpress integrated

--- Frontend/src/components/Blog.js
+++ Frontend/src/components/Blog.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 export default function Blog() {
   return (
     <iframe
+      title="wordpress"
       src="http://sx4707.uta.cloud/15-2"
       style={{ height: "100vh", width: "100%" }}
       frameborder="6"
