2964484113f0e04f3a2f1130206b0ad6b4585b1a
WorkWithSoham <sohamtembe98@gmail.com>
2022-04-19 18:29:09 -0500

    Register email functionality added

--- Backend/app/Http/Controllers/delete.php
+++ Backend/app/Http/Controllers/delete.php
@@ -8,6 +8,23 @@ class delete extends Controller
 {
     public function delete(Request $request)
     {
-        
+        $servername = "utacloud3.reclaimhosting.com";
+        $username = "sst0847_siremar";
+        $password = "Group24sps";
+        $database = "sst0847_backend";
+
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $database);
+
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $tableName = $request['table'];
+        $id = $request['id'];
+        $sql = "DELETE FROM $tableName WHERE id= $id";
+        $result = $conn->query($sql);
+        var_dump($result);
     }
 }
